b1cab6a559801f1dcdfcd8d4efcab808e177d4ce
Colin Sherratt <colin.sherratt@gmail.com>
2014-08-17 01:26:44 -0400

    added cube

---
+++ src/cube.rs
@@ -0,0 +1,49 @@
+
+use core::iter::Range;
+use super::{Generator, Quad, Vector3};
+
+pub struct Cube {
+    range: Range<uint>
+}
+
+impl Cube {
+    pub fn new() -> Cube {
+        Cube {
+            range: range(0, 6)
+        }
+    }
+
+    pub fn vert(&self, idx: uint) -> Vector3<f32> {
+        let x = if idx & 4 == 4 { 1.} else { -1. };
+        let y = if idx & 2 == 2 { 1.} else { -1. };
+        let z = if idx & 1 == 1 { 1.} else { -1. };
+        Vector3([x, y, z])
+    }
+
+    pub fn face(&self, idx: uint) -> Quad<Vector3<f32>> {
+        match idx {
+            // back face
+            0 => Quad::new(self.vert(0b000), self.vert(0b001),
+                           self.vert(0b011), self.vert(0b010)),
+            1 => Quad::new(self.vert(0b110), self.vert(0b111),
+                           self.vert(0b101), self.vert(0b100)),
+            2 => Quad::new(self.vert(0b100), self.vert(0b101),
+                           self.vert(0b001), self.vert(0b000)),
+            3 => Quad::new(self.vert(0b011), self.vert(0b111),
+                           self.vert(0b110), self.vert(0b010)),
+            4 => Quad::new(self.vert(0b000), self.vert(0b010),
+                           self.vert(0b110), self.vert(0b100)),
+            5 => Quad::new(self.vert(0b101), self.vert(0b111),
+                           self.vert(0b011), self.vert(0b001)),
+            idx => fail!("{} face is higher then 6", idx)
+        }
+    }
+} 
+
+impl Iterator<Quad<Vector3<f32>>> for Cube {
+    fn next(&mut self) -> Option<Quad<Vector3<f32>>> {
+        self.range.next().map(|idx| self.face(idx))
+    }
+}
+
+impl Generator<Vector3<f32>, Quad<Vector3<f32>>> for Cube {}
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,121 @@ extern crate core;
 
 use std::iter::FromIterator;
 use core::slice::Items;
+use std::collections::RingBuf;
+use std::collections::Deque;
 
-pub mod generators;
+mod cube;
+
+pub mod generators {
+    pub use cube::Cube;
+}
+
+pub struct Vector1<T>(pub [T, ..1]);
+pub struct Vector2<T>(pub [T, ..2]);
+pub struct Vector3<T>(pub [T, ..3]);
+pub struct Vector4<T>(pub [T, ..4]);
+
+impl<T> FromIterator<T> for Vector1<T> {
+    fn from_iter<I: Iterator<T>>(iter: I) -> Vector1<T> {
+        let mut iter = iter;
+        match iter.next() {
+            Some(a) => Vector1([a]),
+            _ => fail!("should have found 2 vertices to buld a Vector1")
+        }
+    }
+}
+
+impl<T: Clone> Clone for Vector1<T> {
+    fn clone(&self) -> Vector1<T> {
+        let &Vector1(ref v) = self;
+        Vector1([v[0].clone()])
+    }
+}
+
+impl<T> Poly<T> for Vector1<T> {
+    fn as_slice<'a>(&'a self) -> &'a [T] {
+        let &Vector1(ref s) = self;
+        s.as_slice()
+    }
+}
+
+impl<T> FromIterator<T> for Vector2<T> {
+    fn from_iter<I: Iterator<T>>(iter: I) -> Vector2<T> {
+        let mut iter = iter;
+        match (iter.next(), iter.next()) {
+            (Some(a), Some(b)) => Vector2([a, b]),
+            _ => fail!("should have found 2 vertices to buld a Vector2")
+        }
+    }
+}
+
+impl<T: Clone> Clone for Vector2<T> {
+    fn clone(&self) -> Vector2<T> {
+        let &Vector2(ref v) = self;
+        Vector2([v[0].clone(),
+                 v[1].clone()])
+    }
+}
+
+impl<T> Poly<T> for Vector2<T> {
+    fn as_slice<'a>(&'a self) -> &'a [T] {
+        let &Vector2(ref s) = self;
+        s.as_slice()
+    }
+}
+
+impl<T> FromIterator<T> for Vector3<T> {
+    fn from_iter<I: Iterator<T>>(iter: I) -> Vector3<T> {
+        let mut iter = iter;
+        match (iter.next(), iter.next(), iter.next()) {
+            (Some(a), Some(b), Some(c)) => Vector3([a, b, c]),
+            _ => fail!("should have found 3 vertices to buld a Vector3")
+        }
+    }
+}
+
+impl<T: Clone> Clone for Vector3<T> {
+    fn clone(&self) -> Vector3<T> {
+        let &Vector3(ref v) = self;
+        Vector3([v[0].clone(),
+                 v[1].clone(),
+                 v[2].clone()])
+    }
+}
+
+impl<T> Poly<T> for Vector3<T> {
+    fn as_slice<'a>(&'a self) -> &'a [T] {
+        let &Vector3(ref s) = self;
+        s.as_slice()
+    }
+}
+
+impl<T> FromIterator<T> for Vector4<T> {
+    fn from_iter<I: Iterator<T>>(iter: I) -> Vector4<T> {
+        let mut iter = iter;
+        match (iter.next(), iter.next(), iter.next(), iter.next()) {
+            (Some(a), Some(b), Some(c), Some(d)) => Vector4([a, b, c, d]),
+            _ => fail!("should have found 4 vertices to buld a quad")
+        }
+    }
+}
+
+impl<T: Clone> Clone for Vector4<T> {
+    fn clone(&self) -> Vector4<T> {
+        let &Vector4(ref v) = self;
+        Vector4([v[0].clone(),
+                 v[1].clone(),
+                 v[2].clone(),
+                 v[3].clone()])
+    }
+}
+
+impl<T> Poly<T> for Vector4<T> {
+    fn as_slice<'a>(&'a self) -> &'a [T] {
+        let &Vector4(ref s) = self;
+        s.as_slice()
+    }
+}
 
 trait Poly<T> : FromIterator<T> {
     fn as_slice<'a>(&'a self) -> &'a [T];
@@ -14,44 +127,55 @@ trait Poly<T> : FromIterator<T> {
     }
 }
 
-pub struct Trinagle<T>([T, ..3]);
+pub struct Triangle<T>(Vector3<T>);
 
-impl<T> Trinagle<T> {
-    pub fn new(v0: T, v1: T, v2: T) -> Trinagle<T> {
-        Trinagle([v0, v1, v2])
+impl<T> Triangle<T> {
+    pub fn new(v0: T, v1: T, v2: T) -> Triangle<T> {
+        Triangle(Vector3([v0, v1, v2]))
     }
 }
 
-impl<T> FromIterator<T> for Trinagle<T> {
-    fn from_iter<I: Iterator<T>>(iter: I) -> Trinagle<T> {
+impl<T: Clone> Clone for Triangle<T> {
+    fn clone(&self) -> Triangle<T> {
+        let &Triangle(ref v) = self;
+        Triangle(v.clone()) 
+    }
+}
+
+impl<T> FromIterator<T> for Triangle<T> {
+    fn from_iter<I: Iterator<T>>(iter: I) -> Triangle<T> {
         let mut iter = iter;
         match (iter.next(), iter.next(), iter.next()) {
-            (Some(a), Some(b), Some(c)) => Trinagle([a, b, c]),
+            (Some(a), Some(b), Some(c)) => Triangle(Vector3([a, b, c])),
             _ => fail!("should have found 3 vertices to buld a triangle")
         }
     }
 }
 
-impl<T> Poly<T> for Trinagle<T> {
+impl<T> Poly<T> for Triangle<T> {
     fn as_slice<'a>(&'a self) -> &'a [T] {
-        let &Trinagle(ref s) = self;
+        let &Triangle(Vector3(ref s)) = self;
         s.as_slice()
     }
 }
 
-pub struct Quad<T>([T, ..4]);
+pub struct Quad<T>(pub Vector4<T>);
 
 impl<T> Quad<T> {
     pub fn new(v0: T, v1: T, v2: T, v3: T) -> Quad<T> {
-        Quad([v0, v1, v2, v3])
+        Quad(Vector4([v0, v1, v2, v3]))
     }
 }
 
-impl<T: Clone> Quad<T> {
-    pub fn to_triangles(&self) -> [Trinagle<T>, ..2] {
-        let &Quad([ref v0, ref v1, ref v2, ref v3]) = self;
-        [Trinagle::new(v0.clone(), v1.clone(), v2.clone()),
-         Trinagle::new(v2.clone(), v3.clone(), v0.clone())]
+pub trait ToTriangle<T> {
+    fn to_triangles(&self) -> T;
+}
+
+impl<T: Clone> ToTriangle<Vector2<Triangle<T>>> for Quad<T> {
+    fn to_triangles(&self) -> Vector2<Triangle<T>> {
+        let &Quad(Vector4([ref v0, ref v1, ref v2, ref v3])) = self;
+        Vector2([Triangle::new(v0.clone(), v1.clone(), v2.clone()),
+                 Triangle::new(v2.clone(), v3.clone(), v0.clone())])
     }
 }
 
@@ -59,7 +183,7 @@ impl<T> FromIterator<T> for Quad<T> {
     fn from_iter<I: Iterator<T>>(iter: I) -> Quad<T> {
         let mut iter = iter;
         match (iter.next(), iter.next(), iter.next(), iter.next()) {
-            (Some(a), Some(b), Some(c), Some(d)) => Quad([a, b, c, d]),
+            (Some(a), Some(b), Some(c), Some(d)) => Quad(Vector4([a, b, c, d])),
             _ => fail!("should have found 4 vertices to buld a quad")
         }
     }
@@ -67,7 +191,7 @@ impl<T> FromIterator<T> for Quad<T> {
 
 impl<T> Poly<T> for Quad<T> {
     fn as_slice<'a>(&'a self) -> &'a [T] {
-        let &Quad(ref s) = self;
+        let &Quad(Vector4(ref s)) = self;
         s.as_slice()
     }
 }
@@ -80,9 +204,6 @@ trait VertexPass<IN, OUT> {
     }
 }
 
-trait GeometryPass<T, U, IN: Poly<T>, OUT: Poly<U>> {
-    fn geometry_pass(&self, &IN) -> OUT;
-}
 
 pub struct IndexDeref<'a, T> {
     vertices: &'a [T]
@@ -102,13 +223,98 @@ impl<'a, T: Clone> VertexPass<uint, T> for IndexDeref<'a, T> {
     }
 }
 
-trait Generator<T> : Iterator<T> {}
+pub trait Generator<T, P: Poly<T>> : Iterator<P> {
+    fn vertices(self) -> VertexGenerator<Self, T> {
+        VertexGenerator {
+            source: self,
+            spare: Vec::new()
+        }
+    }
+
+    fn geometry<'a, OUT_P>(self, max: uint, f: |P, emit: |OUT_P||:'a) -> GeometryMap<'a, Self, P, OUT_P> {
+        GeometryMap {
+            source: self,
+            max: max,
+            buffer: Some(RingBuf::with_capacity(max)),
+            f: f
+        }
+    }
+}
+
+pub struct GeometryMap<'a, SRC, IN_P, OUT_P> {
+    source: SRC,
+    max: uint,
+    buffer: Option<RingBuf<OUT_P>>,
+    f: |IN_P, |OUT_P||:'a
+}
+
+impl<'a, IN_P, OUT_P, SRC: Iterator<IN_P>> Iterator<OUT_P> for GeometryMap<'a, SRC, IN_P, OUT_P> {
+    fn next(&mut self) -> Option<OUT_P> {
+        loop {
+            match self.buffer.get_mut_ref().pop_front() {
+                p @ Some(_) => return p,
+                None => ()
+            }
+
+            match self.source.next() {
+                Some(p) => {
+                    let mut buf = self.buffer.take().unwrap();
+                    (self.f)(p, |emited| buf.push(emited));
+                    self.buffer = Some(buf);
+                }
+                None => return None
+            }
+
+        }
+    }
+
+    fn size_hint(&self) -> (uint, Option<uint>) {
+        match self.source.size_hint() {
+            (base, Some(s)) => {
+                (base, Some(s*self.max))
+            },
+            (base, None) => {
+                (base, Some(base*self.max))
+            }
+        }
+    }
+}
+
+impl<'a, IN_P, OUT_V, OUT_P: Poly<OUT_V>, SRC: Iterator<IN_P>> Generator<OUT_V, OUT_P> for GeometryMap<'a, SRC, IN_P, OUT_P> {}
+
+impl<'a, IN_V, IN_P: Poly<IN_V>,
+         OUT_V, OUT_P: Poly<OUT_V>,
+         SRC: Iterator<IN_P>> Generator<OUT_V, OUT_P> for core::iter::Map<'a, IN_P, OUT_P, SRC> {}
+
+pub struct VertexGenerator<SRC, T> {
+    source: SRC,
+    spare: Vec<T>
+}
+
+impl<T: Clone, P: Poly<T>, SRC: Iterator<P>> Iterator<T> for VertexGenerator<SRC, T> {
+    fn next(&mut self) -> Option<T> {
+        loop {
+            match self.spare.remove(0) {
+                out @ Some(_) => return out,
+                None => ()
+            }
+
+            match self.source.next() {
+                Some(s) => {
+                    self.spare.push_all(s.as_slice());
+                }
+                None => return None
+            }
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod test {
     use IndexDeref;
     use VertexPass;
-    use Trinagle;
+    use Triangle;
     use Poly;
 
     #[test]
@@ -127,7 +333,7 @@ mod test {
         let a = &[7u, 8, 9, 10];
         let index = IndexDeref::new(a);
 
-        let out: Trinagle<uint> = index.transform_primative(&Trinagle::new(0u, 1, 2));
-        assert_eq!(out.as_slice(), Trinagle::new(7u, 8, 9).as_slice())
+        let out: Triangle<uint> = index.transform_primative(&Triangle::new(0u, 1, 2));
+        assert_eq!(out.as_slice(), Triangle::new(7u, 8, 9).as_slice())
     }
 }
